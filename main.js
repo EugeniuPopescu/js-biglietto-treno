a8d1052a48b50cb3f13ad14696a275d358f9bf91
Eugen <epopescuu@yahoo.it>
2023-10-27 11:27:03 +0200

    adjusted sale & message

--- main.js
+++ main.js
@@ -4,47 +4,56 @@ const price = km * 0.21
 
 let distance;
 let strangeAge;
-let finalPrice; 
-let sale;
+let finalPrice;
+let sale; 
+let saleMessage;
 
-if (!isFinite(km) && !isFinite(age)) {
+// Se eta e distanza NON sono numerici -> messaggio di errore
+if (!Number.isFinite(km) && !Number.isFinite(age)) {
     // error mex
     document.getElementById('message').innerHTML = `Errore nell'inserimento dei dati`;
+
+    // altrimenti partono i controlli
 } else {
     
     // controllo sulla distanza
     if ( km > 1000 ) {
         distance = `Prendi un aereo baby`;
-    }else if ( km < 5 ) {
-        distance = `Camminaaaaaa, pochi km per fare in treno`; 
+
+    } else if ( km < 5 ) {
+        distance = `Camminaaaaaa, pochi km, devi fare almeno 5 km`; 
+
     } else {
         distance = `Hai inserito una distanza percorribile dai nostri treni`;
         
         // controllo eta    
         if (age <= 8 || age >= 99) {
-            strangeAge = `ERRORE Hai inserito un età sbagliata`
+            strangeAge = `ERRORE età deve essere compresa tra 8 e 99 anni`
+
         } else {
-            strangeAge = ` Hai inserito correttamente l'età`;
+            strangeAge = `Hai inserito correttamente l'età`;
             
             // prezzo finale
-            if ( age <= 18 ) {
-                finalPrice = (price * 20)/100;
-                sale = `Sconto under 18`;
+            if ( age < 18 ) {
+                sale = (price / 100) * 20;
+                finalPrice = price - sale;
+                saleMessage = `Sconto under 18 (${price}€ - ${sale.toFixed(2)}€ (sconto))`;
                 
             } else if( age >= 65 ) {
-                finalPrice = (price * 40)/100;
-                sale = `Sconto over 65`;
+                sale = (price / 100) * 40;
+                finalPrice = price - sale;
+                saleMessage = `Sconto over 65 (${price}€ - ${sale.toFixed(2)}€ (sconto))`;
                 
             } else {
                 finalPrice = price;
-                sale = `Non c'è sconto`;
+                saleMessage = `Non c'è sconto`;
             }
         }
+        document.getElementById('age').innerHTML = `Validazione età: ${strangeAge} (${age} anni)`;
     }
+    document.getElementById('distance').innerHTML = `Validazione distanza: ${distance} (${km} Km)`;
+    document.getElementById('finalPrice').innerHTML = `Prezzo Finale: ${finalPrice.toFixed(2)} € => ${saleMessage}`;
 }
-document.getElementById('distance').innerHTML = `Validazione distanza: ${distance} (${km}Km)`;
-document.getElementById('age').innerHTML = `Validazione età: ${strangeAge} (${age} anni)`;
-document.getElementById('finalPrice').innerHTML = `Prezzo Finale: ${finalPrice.toFixed(2)} Euro => ${sale}`;
 
 
 
